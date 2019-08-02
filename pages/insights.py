f8fcaaa14796dd9a8c8672d614433eb34cb7cffa
Eric Wuerfel <Eric@Erics-MacBook-Pro-5.local>
2019-08-02 10:05:37 -0400

    updated insights, process

--- pages/insights.py
+++ pages/insights.py
@@ -12,9 +12,17 @@ column1 = dbc.Col(
             """
         
             ## Insights
-            The figure below shows how your diagnosis and state combine to influence prices.
+            The two-feature Partial Dependence Plot (PDP) to the right shows how your diagnosis and state combine to influence prices.
             
-            From the plot, we can see that Orthopedics (column 6) is consistently the most expensive while Respiratory issues (column 3) are consistently the least expensive.
+            The color of each box represents the expected cost for a given diagnosis in a given state. From the plot, we can see that Orthopedics (column 6) is consistently the most expensive while Respiratory issues (column 3) are consistently the least expensive.
+            
+            Rows 9, 12 and 45 correspond to DC, Hawaii, and Utah. These are a few of the brightest rows in our plot, indicating that the model predicted higher expenses for these states.
+            
+            The most expensive prediction was for an Orthopedic diagnosis in Hawaii, while the least expensive prediction was for a respiratory issue in Mississippi.
+            
+            The single-feature PDP gives us a closer look at how one's diagnosis alone influences your out-of-pocket costs.
+            
+            The minimum at 3 (Respiratory) and maximum at 6 (Orthopedic) confirm our suspicion that Respiratory issues are the least expensive while Orthopedic issues are the most expensive. Surprisingly, Neurological (1) diagnoses didn't come with a considerably higher price than average, while Circulatory (4) and Digestive (5) were slightly expensive.
 
             """
         )
--- pages/process.py
+++ pages/process.py
@@ -13,11 +13,13 @@ column1 = dbc.Col(
         
             ## Process
             
-            This web app uses a random forest regressor to estimate out-of-pocket medical costs based on [Medicare Payment Data](https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Inpatient.html). I hope that this creates a little more transparency in the health care market.
+            This web app uses a random forest regressor to estimate out-of-pocket medical costs based on [Medicare Payment Data](https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Inpatient.html).
             
-            Consumers often have no clue how much a diagnosis or procedure could end up costing them beforehand. The complicated nature of the human body usually means that doctors don't even have a clue how much it might end up costing you. This uncertainty and lack of transparency is just one of the reasons our health care costs in the United States is so high (about 17% of our GDP compared with the average of about 10-12%).
+            Consumers often have no clue how much a diagnosis or procedure could end up costing them beforehand. The complicated nature of the human body usually means that doctors don't even have a clue how much it might end up costing you. This uncertainty and lack of transparency is just one of the reasons our health care costs in the United States are so high (about 17% of our GDP compared with the average of about 10-12%).
             
-            I sorted the top 100 most common diagnoses and procedures into categories that most consumers can understand:
+            This tool takes in a category of medical diagnosis and your health care provider's state, then returns an estimate of the out-of-pocket costs one would expect with Medicare coverage. 
+            
+            I sorted the top 100 most common diagnoses and procedures into general categories:
                 * Cardiac/Circulatory
                 * Cranial/Neurological
                 * Digestive
@@ -25,6 +27,12 @@ column1 = dbc.Col(
                 * Respiratory
                 * Other
 
+            These categories were tuned to be general enough so that one could reasonably guess which category your diagnosis might fall into before getting an official diagnosis, yet detailed enough to capture the wide variance among out-of-pocket costs.
+            
+            Out-of-pocket costs can be found by subtracting medicare coverage from net price for each diagnosis. Using the category of diagnosis and the provider's state, I trained a random forest regressor to predict the out-of-pocket costs.
+            
+            There's a remarkable lack of transparency in the health care system, which is further confounded by the inherent uncertainty in the nature of medicine. Hopefully this tool can provide a bit more information to Medicare beneficiaries about their expected costs.
+            
             """
         ),
 
